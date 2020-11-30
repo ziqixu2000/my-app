113be2c36786fe8d5c81ac7554c7742ce7802ac6
ziqixu2000 <ziqixu@wustl.edu>
2020-11-30 08:49:19 +0800

    hello world

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello world!
         </p>
         <a
           className="App-link"
@@ -19,7 +41,10 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
+
+
 
 export default App;
